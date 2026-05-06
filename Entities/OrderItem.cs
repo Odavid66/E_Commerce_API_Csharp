77a3c9e81720f57a57f6233709289a7053b0f647
Odavid66 <149391081+Odavid66@users.noreply.github.com>
2026-05-06 10:42:49 +0100

    updated the cart,order,cartitem and orderitem entities with their relationships

---
+++ Entities/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace E_commerce_API.Entities
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int UnitPrice { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+    }
+}
